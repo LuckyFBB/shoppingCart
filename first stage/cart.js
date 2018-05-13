538ca164019a288e264f5956c0b19d39ff2c41d1
LuckyFBB <38368040+LuckyFBB@users.noreply.github.com>
2018-05-13 13:55:01 +0800

    创建Vue，实现数据重复渲染

---
+++ first stage/cart.js
@@ -0,0 +1,45 @@
+new Vue({
+	el:"#app",
+	data:{
+		"totalMoney":100,
+		"list":[
+		{
+			"bookId":"200001",
+			"bookName":"目送",
+			"bookPrice":30,
+			"bookQuantity":1,
+			"bookImg":"img/book-1.jpg"
+			},
+		{
+			"bookId":"200002",
+			"bookName":"偷影子的人",
+			"bookPrice":32,
+			"bookQuantity":5,
+			"bookImg":"img/book-2.jpg"
+			},
+		{
+			"bookId":"200001",
+			"bookName":"追风筝的人",
+			"bookPrice":51,
+			"bookQuantity":54,
+			"bookImg":"img/book-3.jpg"
+			},
+		{
+			"bookId":"200001",
+			"bookName":"萤火虫小巷",
+			"bookPrice":20,
+			"bookQuantity":7,
+			"bookImg":"img/book-4.jpg"
+			}
+		]
+	},
+	filters:{
+
+	},
+	mounted:function(){
+
+	},
+	methods:{
+
+	}
+}); 
